4a44d4d701e15eeef8c820b7706f922d172cf70a
Brent Yorgey <byorgey@gmail.com>
2017-06-10 12:18:08 -0500

    WIP: documentation and improvements to Data.Active

--- src/Data/Active.hs
+++ src/Data/Active.hs
@@ -18,7 +18,47 @@
 --
 -----------------------------------------------------------------------------
 
-module Data.Active where
+module Data.Active
+  ( -- * Durations
+
+    Duration(..)
+
+    -- ** Conversion
+
+  , toDuration, fromDuration, fromFDuration
+
+    -- ** Operations
+
+  , addDuration, minDuration
+
+    -- * Active
+
+  , Active
+  , runActive
+
+    -- ** Constructing
+  , active, instant
+  , ui, interval, dur
+
+    -- ** Running/sampling
+
+  , runActive, start, end, simulate
+
+    -- ** Combinators
+
+    -- *** Sequential composition
+  , (->-), (->>), (>>-), (-<>-)
+  , sequenceNE, movie, Horiz(..)
+
+    -- *** Parallel composition
+    -- $par
+  , (<:>), stack
+
+    -- *** Other combinators
+
+  , stretch, stretchTo, matchDuration
+  , cut, backwards, snapshot
+  ) where
 
 import           Data.List.NonEmpty   (NonEmpty (..))
 import qualified Data.List.NonEmpty   as NE
@@ -28,12 +68,23 @@ import           Control.IApplicative
 import           Data.Finitude
 import qualified Data.Vector          as V
 
+-- XXX go through and include diagrams of everything!
+
 ------------------------------------------------------------
--- Time
+-- Durations
 ------------------------------------------------------------
 
+-- | The type of (potentially infinite) /durations/ over a given
+--   numeric type @n@.  The type index @f@ indicates whether the
+--   duration is finite or infinite.  The infinite duration is longer
+--   than any finite duration.
 data Duration f n where
+
+  -- | A finite duration of a given nonnegative length.  The length
+  --   could be zero.
   Duration :: n -> Duration F n
+
+  -- | An infinite duration.
   Forever  ::      Duration I n
 
 deriving instance Show n => Show (Duration f n)
@@ -43,6 +94,31 @@ instance Eq n => Eq (Duration f n) where
   Duration n1 == Duration n2 = n1 == n2
   Forever     == Forever     = True
 
+-- | Note that the 'Ord' instance for 'Duration' is not quite as
+--   useful as one might like, because it forces the type indices to
+--   be the same, so it can only be used to compare two finite or two
+--   infinite durations.  To compare durations in general, use
+--   'compareDuration'.
+instance Ord n => Ord (Duration f n) where
+  compare (Duration n1) (Duration n2) = compare n1 n2
+  compare Forever Forever             = EQ
+
+-- | Compare two durations.
+compareDuration :: Ord n => Duration f1 n -> Duration f2 n -> Ordering
+compareDuration Forever       Forever       = EQ
+compareDuration (Duration _)  Forever       = LT
+compareDuration Forever       (Duration _)  = GT
+compareDuration (Duration n1) (Duration n2) = compare n1 n2
+
+-- XXX make an Additive instance instead of Num?
+
+-- | /Finite/ durations inherit the additive structure of the
+--   underlying numeric type.  (Note that it does not actually make
+--   sense to multiply durations.)  To add durations in general, see
+--   'addDuration'.
+--
+--   This instance also gives us the convenience of 'fromInteger', so
+--   numeric literals can be used as finite durations.
 instance Num n => Num (Duration F n) where
   fromInteger               = toDuration . fromInteger
   negate (Duration n)       = Duration (negate n)
@@ -51,23 +127,27 @@ instance Num n => Num (Duration F n) where
   abs (Duration n)          = Duration (abs n)
   signum (Duration n)       = Duration (signum n)
 
--- | A convenient wrapper function to convert a numeric value into a duration.
+-- | A wrapper function to convert a numeric value into a finite duration.
 toDuration :: n -> Duration F n
 toDuration = Duration
 
--- | A convenient unwrapper function to turn a duration into a numeric value.
+-- | An unwrapper function to turn a duration into a numeric value.
 fromDuration :: Duration f n -> Maybe n
 fromDuration Forever      = Nothing
 fromDuration (Duration n) = Just n
 
+-- | Like 'fromDuration' when you know you have a finite duration.
 fromFDuration :: Duration F n -> n
 fromFDuration (Duration n) = n
 
+-- | Add two durations.  If either one is infinite, so is the result;
+--   finite durations add normally.
 addDuration :: Num n => Duration f1 n -> Duration f2 n -> Duration (Union f1 f2) n
 addDuration Forever      _            = Forever
 addDuration (Duration _) Forever      = Forever
 addDuration (Duration a) (Duration b) = Duration (a + b)
 
+-- | The minimum of two durations.
 minDuration :: (Num n, Ord n) => Duration f1 n -> Duration f2 n -> Duration (Isect f1 f2) n
 minDuration Forever Forever           = Forever
 minDuration Forever (Duration b)      = Duration b
@@ -80,38 +160,168 @@ minDuration (Duration a) (Duration b) = Duration (min a b)
 --  Active
 ------------------------------------------------------------
 
+-- | A value of type @Active n f a@ is a time-varying value of type
+--   @a@ with a given duration.
+--
+--   * @f@ is an index indicating whether the duration is finite or
+--   infinite.
+--   * @n@ is the numeric type used for durations.  Any numeric type
+--     can be used, although it makes the most sense for types with
+--     decidable equality, such as 'Rational'.
+--   * @a@ is the type of the values.
+--
+--   If the duration is infinite, it can be thought of as a function
+--   \( [0,+\infty) \to a \); if it has a finite duration \( d \), it
+--   can be thought of as a function \( [0,d] \to a \) (note in
+--   particular that the interval is /closed/ on both ends: the
+--   function is defined at \(0\) as well as at the duration \(d\)).
+--
+--   @Active n f@ is a @Functor@, and @Active n@ is an 'IFunctor' and
+--   'IApplicative' (the 'IApplicative' instance in particular is how
+--   one does parallel composition of actives).  XXX mention Semigroup
+--
+--   This definition is intentionally abstract, since the
+--   implementation may change in the future to enable additional
+--   optimizations.  To construct an 'Active' value, see the 'active'
+--   function as well as 'ui', 'interval', 'dur', 'discrete',
+--   and the various combinators.
 data Active :: * -> Finitude -> * -> * where
   Active   :: Duration f n -> (n -> a) -> Active n f a
+  deriving Functor
 
+-- | Constructor for 'Active' values, given a duration \(d\) and a
+--   function from \([0,d] \to a\).  The given function need not be
+--   defined outside the interval, although in Haskell there is no way
+--   to indicate this in the types.  For example,
+--
+--   > active 3 (\d -> if d <= 3 then d*2 else error "o noes!")
+--
+--   is a well-defined, total 'Active' value.
+active :: Duration f n -> (n -> a) -> Active n f a
+active = Active
+
+-- | A value of duration zero.
+--
+--   @'instant' a = 'active' 0 (const a)@.
+instant :: Num n => a -> Active n F a
+instant a = active 0 (const a)
+
+-- | The semantic function for 'Active': interpret an 'Active' value
+--   as a function from durations.  Looked at another way, this is how
+--   you can sample an 'Active' value at a given duration.  Note that
+--   attempting to evaluate a finite active past its duration results
+--   in a runtime error. (Unfortunately, in Haskell it would be very
+--   difficult to rule this out statically.)
+runActive :: Ord n => Active n f a -> n -> a
+runActive (Active Forever f) t = f t
+runActive (Active (Duration d) f) t
+  | t > d     = error "Active value evaluated past its duration."
+  | otherwise = f t
+
+-- | The unit interval: the identity function on the interval \( [0,1] \).
 ui :: Num n => Active n F n
 ui = Active 1 id
 
+-- | @interval a b@ varies linearly from \( a \) to \( b \) over a
+--   duration of \( b - a \).  That is, it represents the function \( d \mapsto a + d \).
 interval :: Num n => n -> n -> Active n F n
 interval a b = Active (toDuration (b - a)) (a+)
 
+-- | @dur@ is the infinite active value representing the function
+--   \( d \mapsto d \).  It is called @dur@ since it can be thought of as
+--   representing "the current duration" at any point in time.
 dur :: Active n I n
 dur = Active Forever id
 
 infixr 4 ->>
-(->>) :: (Semigroup a, Num n, Ord n) => Active n F a -> Active n f a -> Active n f a
-(Active d1@(Duration n1) f1) ->> (Active d2 f2) = Active (addDuration d1 d2) f
+
+-- | Sequential composition.
+--
+--   @x ->- y@ is the active which behaves first as @x@, and then as
+--   @y@; the total duration is the sum of the durations of @x@ and
+--   @y@.  The value of @x ->- y@ at the instant @x@ and @y@ overlap
+--   is the composition of @x@ and @y@'s values under ('<>').
+--
+--   Note that @x@ must be finite, but @y@ may be infinite.
+--
+--   See also ('-<>-'), where the final value of @x@ "accumulates".
+--   In the case that the values of @x@ and @y@ do not have a
+--   'Semigroup' instance, one can, for example, wrap them in 'Last',
+--   so that the value from @x@ will be ignored and the value from @y@
+--   taken. In fact, the ('->>') and ('>>-') operators are provided
+--   for convenience which handle this common situation.
+--
+--   Finite active values form a semigroup under horizontal
+--   composition as long as the value type @a@ is a 'Semigroup';
+--   additionally, if @a@ is a 'Monoid', then finite active values are
+--   as well, with @'instant' 'mempty'@ as the identity.  However, the
+--   'Semigroup' and 'Monoid' instances for 'Active' are for parallel
+--   rather than sequential composition.  The instances with
+--   sequential composition are instead defined for the 'Horiz'
+--   newtype wrapper.
+(->-) :: (Semigroup a, Num n, Ord n) => Active n F a -> Active n f a -> Active n f a
+(Active d1@(Duration n1) f1) ->- (Active d2 f2) = Active (addDuration d1 d2) f
   where
     f n | n <  n1   = f1 n
         | n == n1   = f1 n <> f2 0
         | otherwise = f2 (n - n1)
 
-infix 4 ->-
-(->-) :: (Semigroup a, Num n, Ord n) => Active n F a -> Active n f a -> Active n f a
-(Active d1@(Duration n1) f1) ->- (Active d2 f2) = Active (addDuration d1 d2) f
+-- XXX make more efficient version?
+-- | Sequential composition, preferring the value from the right-hand
+--   argument at the instant of overlap; see ('->-').
+--
+--   XXX example / (picture)
+(->>) :: (Num n, Ord n) => Active n F a -> Active n f a -> Active n f a
+a1 ->> a2 = getLast <$> ((Last <$> a1) ->- (Last <$> a2))
+
+-- | Sequential composition, preferring the value from the left-hand
+--   argument at the instant of overlap; see ('->-').
+(>>-) :: (Num n, Ord n) => Active n F a -> Active n f a -> Active n f a
+a1 >>- a2 = getFirst <$> ((First <$> a1) ->- (First <$> a2))
+
+infix 4 -<>-
+-- | Accumulating sequential composition.
+--
+--   @x -<>- y@ first behaves as @x@, and then behaves as @y@, except
+--   that the final value of @x@ is combined via ('<>') with every
+--   value from @y@.  For example:
+--
+--   XXX look into actual executable doc examples
+--
+--   > > let x = active 3 Sum :: Active Rational F (Sum Rational)
+--   > > let a1 = x ->- x
+--   > > let a2 = x -<>- x
+--   > > map (numerator . getSum . runActive a1) [0 .. 6]
+--   >   [0,1,2,3,1,2,3]
+--   > > map (numerator . getSum . runActive a2) [0 .. 6]
+--   >   [0,1,2,3,4,5,6]
+--
+--   @(-<>-)@ satisfies the law:
+--
+--   @x -<>- y = x ->> (('end' x '<>') '<$>' y)@
+(-<>-) :: (Semigroup a, Num n, Ord n) => Active n F a -> Active n f a -> Active n f a
+(Active d1@(Duration n1) f1) -<>- (Active d2 f2) = Active (addDuration d1 d2) f
   where
     f n | n < n1  = f1 n
         | n >= n1 = f1 n1 <> f2 (n - n1)
 
+-- XXX change the name of Horiz
+
+-- | A newtype wrapper for finite 'Active' values.  The 'Semigroup'
+--   and 'Monoid' instances for this wrapper use sequential rather
+--   than parallel composition.
 newtype Horiz n a = Horiz { getHoriz :: Active n F a }
 
 instance (Num n, Ord n, Semigroup a) => Semigroup (Horiz n a) where
-  Horiz a1 <> Horiz a2 = Horiz (a1 ->> a2)
+  Horiz a1 <> Horiz a2 = Horiz (a1 ->- a2)
 
+instance (Num n, Ord n, Monoid a, Semigroup a) => Monoid (Horiz n a) where
+  mempty = Horiz (instant mempty)
+  mappend = (<>)
+
+-- | Sequence a nonempty list of finite actives together, via ('->-'),
+--   but using a balanced fold (which can be more efficient than the
+--   usual linear fold).
 sequenceNE :: (Num n, Ord n, Semigroup a) => NonEmpty (Active n F a) -> Active n F a
 sequenceNE = getHoriz . foldB1 . NE.map Horiz
 
@@ -129,16 +339,19 @@ foldB1 (a :| as) = maybe a (a <>) (foldBM as)
     pair _   [a]        = [a]
     pair (&) (a1:a2:as) = (a1 & a2) : pair (&) as
 
--- For convenience
+-- | A variant of 'sequenceNE' defined on lists instead of 'NonEmpty'
+--   for convenience; @movie []@ is a runtime error.
 movie :: (Num n, Ord n, Semigroup a) => [Active n F a] -> Active n F a
 movie []     = error "Can't make empty movie!"
 movie (a:as) = sequenceNE (a :| as)
 
-startVal :: Num n => Active n f a -> a
-startVal (Active _ f) = f 0
+-- | Extract the value at the beginning of an 'Active'.
+start :: Num n => Active n f a -> a
+start (Active _ f) = f 0
 
-endVal :: Active n F a -> a
-endVal (Active (Duration d) f) = f d
+-- | Extract the value at the end of a finite 'Active'.
+end :: Active n F a -> a
+end (Active (Duration d) f) = f d
 
 stretch :: (Fractional n, Ord n) => n -> Active n F a -> Active n F a
 stretch s a@(Active (Duration d) f)
@@ -148,11 +361,6 @@ stretch s a@(Active (Duration d) f)
 
 backwards :: Num n => Active n F a -> Active n F a
 backwards (Active (Duration d) f) =  Active (Duration d) (f . (d-))
-
-runActive :: Ord n => Active n f a -> n -> a
-runActive (Active (Duration d) f) t
-  | t > d     = error "Active evaluated past its duration"
-  | otherwise = f t
 
 matchDuration :: (Ord n, Fractional n) => Active n f a -> Active n f a -> Active n f a
 matchDuration a@(Active (Duration d1) _) (Active (Duration d2) _) = stretch (d2/d1) a
@@ -172,10 +380,14 @@ discrete xs = (Active 1 f)
 snapshot :: (Fractional n) => n -> Active n f a -> Active n I a
 snapshot t (Active _ f) = Active Forever (const (f t))
 
+-- | XXX document me
 simulate :: (Eq n, Fractional n, Enum n) => n -> Active n f a -> [a]
 simulate 0 _ = error "Frame rate can't equal zero"
 simulate n (Active (Duration d) f) = map f [0, 1/n .. d]
 simulate n (Active Forever      f) = map f [0, 1/n ..]
+
+-- $par
+-- Parallel composition!!
 
 instance (Num n, Ord n) => IApplicative (Active n) where
   type Id = I

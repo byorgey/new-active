15fc48d300a71380fbcaa1bd4f1fff5865e73f03
Brent Yorgey <byorgey@gmail.com>
2017-06-10 12:17:51 -0500

    documentation for IApplicative

--- src/Control/IApplicative.hs
+++ src/Control/IApplicative.hs
@@ -3,37 +3,68 @@
 {-# LANGUAGE TypeFamilies   #-}
 {-# LANGUAGE TypeOperators  #-}
 
-module Control.IApplicative where
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.IApplicative
+-- Copyright   :  2016-2017 XXX
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  diagrams-discuss@groups.google.com
+--
+-- Monoidally indexed applicative functors.
+-----------------------------------------------------------------------------
 
--- | Monoidally indexed functors.  @imap@ preserves the index.
+module Control.IApplicative
+  ( -- * Indexed functors
+    IFunctor(..), (<:$>)
+
+    -- * Indexed applicative functors
+  , IApplicative(..), iliftA2
+
+    -- * Lifting non-indexed (applicative) functors
+  , Ixed(..)
+
+  ) where
+
+-- | Functors indexed by an arbitrary kind.  The map operation
+--   (@imap@) is index-preserving.
 class IFunctor (f :: k -> * -> *) where
   imap :: (a -> b) -> f i a -> f i b
 
--- | A convenient synonym for 'imap'.
+-- | An infix synonym for 'imap'.
 (<:$>) :: IFunctor f => (a -> b) -> f i a -> f i b
 (<:$>) = imap
 
--- | Monoidally indexed applicative functors.
+-- | Applicative functors, indexed by a type-level monoid (of
+--   arbitrary kind).  'ipure' and '(<:*>)' at the value level are
+--   mirrored by the monoid identity and combining operation on the
+--   indices at the type level.  The monoid laws on the type indices
+--   are exactly what are needed to ensure the Applicative laws.
 class IFunctor f => IApplicative (f :: k -> * -> *) where
-  -- | The identity element of a type-level monoid.
+
+  -- | The identity element for the type-level index monoid.
   type Id :: k
-  -- | The associative combining operation of a type-level monoid.
+
+  -- | The associative combining operation for the type-level index
+  -- monoid.
   type (:*:) (a :: k) (b :: k) :: k
 
-  -- | 'ipure' creates a value with an identity type
-  --   index.
+  -- | 'ipure' is the indexed analoge to 'pure'; the value it creates
+  --   has the identity element as the type index.
   ipure :: a -> f Id a
 
-  -- | '(<:*>)' combines type indices.
+  -- | '(<:*>)' is the indexed analogue to '(<*>)', where the type
+  --   indices are combined according to the operation for the
+  --   type-level monoid.
   (<:*>) :: f i (a -> b) -> f j a -> f (i :*: j) b
 
+-- | An indexed analogue to 'liftA2'.
 iliftA2 :: IApplicative f => (a -> b -> c) -> f i a -> f j b -> f (i :*: j) c
 iliftA2 g x y = g <:$> x <:*> y
 
 ------------------------------------------------------------
 -- Turning normal functors into indexed ones
 
--- | Turn a normal functor into an indexed one.
+-- | Lift a normal functor into a (trivially) indexed one.
 newtype Ixed f (i :: *) (a :: *) = Ixed (f a)
   deriving Show
 
